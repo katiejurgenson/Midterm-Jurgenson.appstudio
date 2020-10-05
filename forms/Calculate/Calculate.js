56daaeef45697473cad420685941d0166c43953e
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-10-05 16:42:08 -0500

    Random changes
    Random updates

---
+++ forms/Calculate/Calculate.js
@@ -0,0 +1,12 @@
+/* 
+let firstNum = Number(prompt("Please give me a number."));
+let secNum = Number(prompt("Please give me another number."));
+let thrdNum = Number(prompt("Please give me one more number."));
+let finalAnswer = calcAvgSquare(firstNum, secNum, thrdNum);
+
+function calcAvgSquare(num1, num2, num3) {
+  return (((num1 + num2 + num3)/3) * num1**2);
+} 
+
+alert(`The answer is ${finalAnswer}.`)
+*/ 
---
+++ forms/IncomeBracket/IncomeBracket.js
@@ -0,0 +1,22 @@
+/*
+btnSubmit.onclick=function(){
+  let incomeAmt = Number(inptIncome.value)
+  let taxBracket = ""
+  
+  if (incomeAmt <= 30000) {
+    taxBracket = "8%"
+  } else if (incomeAmt > 30000 && incomeAmt <= 99999) {
+    taxBracket = "15%"
+  } else if (incomeAmt > 99999) {
+    taxBracket = "25%"
+  }
+  
+  lblAnswer.value = `With your income of $${incomeAmt}, you are in a tax bracket of ${taxBracket}.`
+}
+
+
+btnAgain.onclick=function(){
+  inptIncome.value = ""
+  lblAnswer.value = ""
+}
+*/ 
