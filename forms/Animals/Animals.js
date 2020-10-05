ac377685e2175583fd3236d4eac0d7c8a1c3d9c3
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-10-05 15:54:10 -0500

    WIP-Animal
    Work in progress on Animal branch

---
+++ forms/Animals/Animals.js
@@ -0,0 +1,5 @@
+let myAnimals = ["dog", "cat", "horse", "meerkat"]
+let newAnimal = prompt("Please give me the name of an animal.").toLowerCase()
+myNewAnimals = myAnimals.push(newAnimal)
+console.log(myNewAnimals)
+// alert(`The last animal is a/an ${myAnimals[myAnimals.length - 1]}`)
