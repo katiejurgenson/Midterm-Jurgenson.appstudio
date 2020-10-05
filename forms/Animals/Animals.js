c566c02cc30b09feb9c59ee3d7f7d42b22e18280
Katie Jurgenson <katiejurgenson@users.noreply.github.com>
2020-10-05 16:48:03 -0500

    Final adjustments
    Final adjustments for submission

--- forms/Animals/Animals.js
+++ forms/Animals/Animals.js
@@ -2,6 +2,5 @@
 let myAnimals = ["dog", "cat", "horse", "meerkat"]
 let newAnimal = prompt("Please give me the name of an animal.").toLowerCase()
 myNewAnimals = myAnimals.push(newAnimal)
-console.log(myNewAnimals)
-// alert(`The last animal is a/an ${myAnimals[myAnimals.length - 1]}`)
+alert(`The last animal is a/an ${myAnimals[myAnimals.length - 1]}`)
 */
